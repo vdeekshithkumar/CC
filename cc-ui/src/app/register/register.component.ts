31f563ca560779be021fbfb241fabc0ab7447488
IVbhavish <128351571+IVbhavish@users.noreply.github.com>
2023-03-24 09:42:20 +0530

    Auto country and corresponding city recommendation in register component

--- cc-ui/src/app/register/register.component.ts
+++ cc-ui/src/app/register/register.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Country, State, City, ICity, ICountry }  from 'country-state-city';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
-  membershipForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    address: new FormGroup({
-      streetAddress: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zipCode: new FormControl('')
-    })
-  });
-  
+export class RegisterComponent implements OnInit {
 
+  countryName:string="";
+  phoneCode:string="";
+  ngOnInit(): void {
+  }
+
+  getCities(){
+    const countries = Country.getAllCountries();
+    const country = countries.find(country => country.name === this.countryName);
+    
+    let iso= country?.isoCode|| '';
+    this.phoneCode = country?.phonecode || '';
+    const allCityDetails= City.getCitiesOfCountry(iso);
+    return allCityDetails?.map((city)=>city.name);
+
+  }
+
+ 
+  getCountries(){
+    const countries = Country.getAllCountries();
+    const countryNames = countries.map((country) => country.name);
+
+    // Find a specific country by name
+    return countryNames;
+  }
 }
+
