9b393b3be1ccd8c38730f6825127045227c4f83c
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2023-04-24 12:59:31 +0530

    inventory surplus updated

--- CC_api/Business/InventoryBusiness.cs
+++ CC_api/Business/InventoryBusiness.cs
@@ -22,37 +22,50 @@ namespace CC_api.Business
         this.inventoryRepository = new InventoryRepository();
       }
 
- 
-
-      
-
-      public async Task<IActionResult> UploadInventory(Inventory inventory)
-      {
-      var inv = new Inventory();
-
- 
-
-      inv.date_created = inventory.date_created;
-      inv.last_modified = inventory.last_modified;
-      inv.company_id = inventory.company_id;
-      inv.container_type = inventory.container_type;
-      inv.available = inventory.available;
-      inv.maximum = inventory.maximum;
-      inv.minimum = inventory.minimum;
-      inv.port_id = inventory.port_id;
-      inv.updated_by = inventory.updated_by;
-      inv.container_size=inventory.container_size;
-
- 
 
 
-      await inventoryRepository.UploadI(inv);
-        return new OkResult();
 
- 
 
-      }
-    public async Task<IActionResult> DeleteInventory(int id)
+    public async Task<IActionResult> UploadInventory(Inventory inventory)
+    {
+      var inv = new Inventory();
+
+
+
+      inv.date_created = inventory.date_created;
+      inv.last_modified = inventory.last_modified;
+      inv.company_id = inventory.company_id;
+      inv.container_type = inventory.container_type;
+      inv.available = inventory.available;
+      inv.maximum = inventory.maximum;
+      inv.minimum = inventory.minimum;
+      if (inventory.available - inventory.maximum > 0)
+      {
+        inv.surplus = inventory.available - inventory.maximum;
+        inv.deficit = 0;
+      }
+      else if (inventory.available - inventory.minimum < 0)
+      {
+        inv.deficit = inventory.minimum - inventory.available;
+        inv.surplus = 0;
+      }
+      else
+      {
+        inv.surplus = 0;
+        inv.deficit = 0;
+
+      }
+      inv.port_id = inventory.port_id;
+      inv.updated_by = inventory.updated_by;
+      inv.container_size = inventory.container_size;
+      await inventoryRepository.UploadI(inv);
+      return new OkResult();
+
+    }
+
+
+
+    public async Task<IActionResult> DeleteInventory(int id)
     {
       var inv = await inventoryRepository.GetInventoryById(id);
       if (inv == null)
@@ -68,7 +81,8 @@ namespace CC_api.Business
 
 
 
-    public async Task<IActionResult> AddExcelData(List<Inventory> excelData,int user_id,int company_id)
+
+    public async Task<IActionResult> AddExcelData(List<Inventory> excelData, int user_id, int company_id)
     {
       await inventoryRepository.DeleteAllInventory();
 
@@ -95,6 +109,22 @@ namespace CC_api.Business
           inv.available = item.available;
           inv.maximum = item.maximum;
           inv.minimum = item.minimum;
+          if (item.available - item.maximum > 0)
+          {
+            inv.surplus = item.available - item.maximum;
+            inv.deficit = 0;
+          }
+          else if (item.available - item.minimum < 0)
+          {
+            inv.deficit = item.minimum - item.available;
+            inv.surplus = 0;
+          }
+          else
+          {
+            inv.surplus = 0;
+            inv.deficit = 0;
+
+          }
           inv.port_id = item.port_id;
           inv.updated_by = user_id;
           inv.container_size = item.container_size;
@@ -108,8 +138,9 @@ namespace CC_api.Business
       }
 
 
- 
+
     }
+
 
 
     public async Task<Inventory> GetInventoryById(int id)
@@ -125,31 +156,47 @@ namespace CC_api.Business
 
 
     public async Task<IActionResult> EditInventory(int id, Inventory inventory)
-    {
-      var inv = await inventoryRepository.GetInventoryById(id);
-      if (inv == null)
-      {
-        return new NotFoundResult();
-      }
+    {
+      var inv = await inventoryRepository.GetInventoryById(id);
+      if (inv == null)
+      {
+        return new NotFoundResult();
+      }
 
- 
 
-      inv.last_modified = DateTime.Now;
-      inv.company_id = inventory.company_id;
-      inv.container_type = inventory.container_type;
-      inv.available = inventory.available;
-      inv.maximum = inventory.maximum;
-      inv.minimum = inventory.minimum;
-      inv.port_id = inventory.port_id;
-      inv.updated_by = inventory.updated_by;
-      inv.container_size = inventory.container_size;
 
- 
+      inv.last_modified = DateTime.Now;
+      inv.company_id = inventory.company_id;
+      inv.container_type = inventory.container_type;
+      inv.available = inventory.available;
+      inv.maximum = inventory.maximum;
+      inv.minimum = inventory.minimum;
+      if (inventory.available - inventory.maximum > 0)
+      {
+        inv.surplus = inventory.available - inventory.maximum;
+        inv.deficit = 0;
+      }
+      else if (inventory.available - inventory.minimum < 0)
+      {
+        inv.deficit = inventory.minimum - inventory.available;
+        inv.surplus = 0;
+      }
+      else
+      {
+        inv.surplus = 0;
+        inv.deficit = 0;
 
-      await inventoryRepository.EditInventoryAsync(inv);
-      return new OkResult();
-    }
-    public async Task<List<Inventory>> GetAllInventoryAsync()
+      }
+      inv.port_id = inventory.port_id;
+      inv.updated_by = inventory.updated_by;
+      inv.container_size = inventory.container_size;
+
+
+
+      await inventoryRepository.EditInventoryAsync(inv);
+      return new OkResult();
+    }
+    public async Task<List<Inventory>> GetAllInventoryAsync()
     {
       return await inventoryRepository.GetAllInventoryAsync();
     }
