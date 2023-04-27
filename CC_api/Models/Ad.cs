7590e83c8184a0b691d3729f83cf9838b28f5070
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2023-04-28 01:34:09 +0530

    ad initial

--- CC_api/Models/Ad.cs
+++ CC_api/Models/Ad.cs
@@ -1,7 +1,18 @@
-/*namespace CC_api.Models
+namespace CC_api.Models
 {
   public class Ad
   {
+    //public int Id { get; set; }
+    public string port_of_arrival { get; set; }
+    public string port_of_departure{ get; set;}
+    public DateTime from_date { get; set; }
+    public string type { get; set; }
+    public decimal price { get; set; }
+    public int free_days { get; set;}
+    public int per_diem { get; set;}
+    public int pick_up_charges { get; set;}
+    public string content { get; set; }
+    public string file { get; set;}
   }
 }
-*/
+
