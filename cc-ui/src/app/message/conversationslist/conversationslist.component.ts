1e9f084e00b3483747885d9e02b218a68d1c51ff
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2023-06-04 01:10:27 +0530

    msges update

--- cc-ui/src/app/message/conversationslist/conversationslist.component.ts
+++ cc-ui/src/app/message/conversationslist/conversationslist.component.ts
@@ -23,11 +23,18 @@ export class ConversationslistComponent {
     activeTab: number = 1;
 
     usersWithProfile = [
-      { name: 'Robert', profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'we can buy 300 20DC containers with 30$ per container',company:'SRS Systems LTD ' },
-      { name: 'Katrin ', profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'Buy deed is for 20DC containers with 30$ and sell is for 40$',company:'HLM Systems LTD ' },
-      { name: 'Sathish', profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'Here the negotiation is not agreed and contract cannot be signed',company:'BHAR Systems LTD ' },
+      { name: 'Robert',online: true,profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'we can buy 300 20DC containers with 30$ per container',company:'SRS Systems LTD ' },
+      { name: 'Katrin ',online: true, profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'Buy deed is for 20DC containers with 30$ and sell is for 40$',company:'HLM Systems LTD ' },
+      { name: 'Sathish',online: false, profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'Here the negotiation is not agreed and contract cannot be signed',company:'BHAR Systems LTD ' },
       // Add more users with profile details here
     ];
+    selectedUser: any; // Replace 'any' with the type of your user object
+
+  // Method to handle user selection
+  selectUser(user: any) {
+    this.selectedUser = user;
+  }
+ 
   
     usersWithoutProfile = [
       { name: 'Robert', message: 'Negotiation for sale' },
@@ -37,5 +44,12 @@ export class ConversationslistComponent {
     selectTab(tab: number) {
       this.activeTab = tab;
     }
+    
+    
+  getCurrentTime() {
+    const currentTime = new Date();
+    const formattedTime = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    return formattedTime;
+  }
 }
 
