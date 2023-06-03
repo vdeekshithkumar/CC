bdf5fc60f33b8b196f81e0d47f8d60b1605fa643
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2023-06-03 15:20:34 +0530

    msg

--- cc-ui/src/app/message/conversationslist/conversationslist.component.ts
+++ cc-ui/src/app/message/conversationslist/conversationslist.component.ts
@@ -23,9 +23,9 @@ export class ConversationslistComponent {
     activeTab: number = 1;
 
     usersWithProfile = [
-      { name: 'Robert', profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'Sold 30 containers' },
-      { name: 'Katrin ', profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'Buy deed is' },
-      { name: 'Sathish', profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'Here the negotiation' },
+      { name: 'Robert', profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'we can buy 300 20DC containers with 30$ per container',company:'SRS Systems LTD ' },
+      { name: 'Katrin ', profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'Buy deed is for 20DC containers with 30$ and sell is for 40$',company:'HLM Systems LTD ' },
+      { name: 'Sathish', profilePhoto: '../assets/images/IMG_20230213_135109.jpg', message: 'Here the negotiation is not agreed and contract cannot be signed',company:'BHAR Systems LTD ' },
       // Add more users with profile details here
     ];
   
