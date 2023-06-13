52ccfa665b43d9169c809e7503d0ec4e2c4bd641
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2023-06-13 11:23:46 +0530

    new profile

--- cc-ui/src/app/app-routing.module.ts
+++ cc-ui/src/app/app-routing.module.ts
@@ -65,7 +65,7 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
-    component:ProfileComponent,
+    component:TemplateComponent,
     path:'profile',
     canActivate: [AuthGuard]
   },
