956a3203ab021914b46102e73829552c0c5286b7
IVProject1 <128127482+IVProject1@users.noreply.github.com>
2023-06-27 12:59:18 +0530

    Advertisement-map-view-changes

--- cc-ui/src/app/view-other-ads/view-other-ads-map-view/view-other-ads-map-view.component.ts
+++ cc-ui/src/app/view-other-ads/view-other-ads-map-view/view-other-ads-map-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { ForecastingService } from 'src/app/forecasting/forecasting.service';
+import { Advertisement, ViewOtherAdsService } from '../view-other-ads.service';
 export interface Port {
   port_id: number;
   company_id: number;
@@ -22,10 +23,13 @@ arrivalLat: number = 0;
 arrivalLng: number = 0;
 
   port: Port[] = [];
+ 
   @Input() selectedDeparturePort: any;
   @Input() selectedArrivalPort: any;
-  @Input() selectedType: any;
-  @Input() selectedSize: any;
+  @Input() adtype: any;
+  @Input() containerTypeId: any;
+  ads: Advertisement[] = [];
+  userOS:any;
   mapId: string = '2b03aff8b2fb72a3'; // Replace with your Map ID
   ports: any[] = [];
   map: google.maps.Map | undefined;
@@ -36,14 +40,33 @@ arrivalLng: number = 0;
   latitude: any;
   longitude: any;
   polyline: google.maps.Polyline | null = null;
+  company_id: any;
+  ad: any;
  
 
-  constructor(private forecastService: ForecastingService) {}
-
-  ngOnInit(): void {
-    this.getAllPorts();
+  constructor(private forecastService: ForecastingService,private adsService:ViewOtherAdsService) {
+    this.userOS = navigator.platform;
+    
   }
 
+  ngOnInit(): void {
+    console.log("dh",this.userOS);
+    this.getAllPorts();
+    this.getAdvertisement();
+  }
+  getAdvertisement() {
+    this.adsService.getAdvertisement(this.company_id).subscribe(
+      (adsdata: Advertisement[]) => {
+        this.ads = adsdata;
+        console.log("From map view", this.ads);
+        this.markPortsOnMap(); // Call markPortsOnMap() after retrieving the advertisements
+      },
+      (error: any) => {
+        console.error('Error fetching advertisements:', error);
+      }
+    );
+  }
+  
   getAllPorts() {
     debugger;
     this.forecastService.getAllPorts().subscribe(
@@ -127,66 +150,109 @@ arrivalLng: number = 0;
   
   
   
-markPortsOnMap() {
-  this.clearMarkers();
+  markPortsOnMap() {
+    this.clearMarkers();
+  
+    if (!this.map || !this.selectedDeparturePort || !this.selectedArrivalPort || !this.adtype) {
+      return;
+    }
+  
+    const departurePort = this.ports.find(port => port.port_name === this.selectedDeparturePort);
+    const arrivalPort = this.ports.find(port => port.port_name === this.selectedArrivalPort);
+  
+    if (!departurePort || !arrivalPort) {
+      return;
+    }
+    const matchingAds = [];
+    for (const ad of this.ads) {
+      if (
+        ad.type_of_ad === this.adtype.toLowerCase() &&
+        ad.port_of_departure === this.selectedDeparturePort.toLowerCase() &&
+        ad.port_of_arrival === this.selectedArrivalPort.toLowerCase() &&
+        ad.container_type_id === this.containerTypeId
+      ) {
+        matchingAds.push(ad);
+      }
+    }
+    console.log('Matching Ads:', matchingAds);
+    
+    
+  
+   
+  
+    if (matchingAds.length === 0) {
+      // No matching ads found, mark all locations on the map
+      this.ports.forEach(port => {
+        const latLng = new google.maps.LatLng(port.latitude, port.longitude);
+  
+        const marker = new google.maps.Marker({
+          position: latLng,
+          title: port.port_name,
+          map: this.map,
+          icon: {
+            url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+          },
+        });
+  
+        this.markers.push(marker);
+      });
+  
+      return;
+    }
+  
+ 
 
-  if (!this.map || !this.selectedDeparturePort || !this.selectedArrivalPort) {
-    return;
+  
+    // Matching ads found, mark only departure and arrival locations
+    const departureLatLng = new google.maps.LatLng(departurePort.latitude, departurePort.longitude);
+    const arrivalLatLng = new google.maps.LatLng(arrivalPort.latitude, arrivalPort.longitude);
+  
+    const departureMarker = new google.maps.Marker({
+      position: departureLatLng,
+      title: this.selectedDeparturePort,
+      map: this.map,
+      icon: {
+        url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+      },
+    });
+  
+    const arrivalMarker = new google.maps.Marker({
+      position: arrivalLatLng,
+      title: this.selectedArrivalPort,
+      map: this.map,
+      icon: {
+        url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+      },
+    });
+  
+    this.markers.push(departureMarker);
+    this.markers.push(arrivalMarker);
+  
+    const bounds = new google.maps.LatLngBounds();
+    bounds.extend(departureMarker.getPosition()!);
+    bounds.extend(arrivalMarker.getPosition()!);
+    this.map.fitBounds(bounds);
+  
+    if (this.polyline) {
+      this.polyline.setMap(null); // Remove the previous polyline if it exists
+    }
+  
+    const polylineOptions = {
+      path: [departureLatLng, arrivalLatLng],
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2,
+    };
+  
+    this.polyline = new google.maps.Polyline(polylineOptions);
+    if (this.polyline) {
+      this.polyline.setMap(this.map!);
+    }
   }
-
-  const departurePort = this.ports.find(port => port.port_name === this.selectedDeparturePort);
-  const arrivalPort = this.ports.find(port => port.port_name === this.selectedArrivalPort);
-
-  if (!departurePort || !arrivalPort) {
-    return;
-  }
-
-  const departureLatLng = new google.maps.LatLng(departurePort.latitude, departurePort.longitude);
-  const arrivalLatLng = new google.maps.LatLng(arrivalPort.latitude, arrivalPort.longitude);
-
-  const departureMarker = new google.maps.Marker({
-    position: departureLatLng,
-    title: this.selectedDeparturePort,
-    map: this.map,
-    icon: {
-      url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-    },
-  });
-
-  const arrivalMarker = new google.maps.Marker({
-    position: arrivalLatLng,
-    title: this.selectedArrivalPort,
-    map: this.map,
-    icon: {
-      url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-    },
-  });
-
-  this.markers.push(departureMarker);
-  this.markers.push(arrivalMarker);
-
-  const bounds = new google.maps.LatLngBounds();
-  bounds.extend(departureMarker.getPosition()!);
-  bounds.extend(arrivalMarker.getPosition()!);
-  this.map.fitBounds(bounds);
-
-  if (this.polyline) {
-    this.polyline.setMap(null); // Remove the previous polyline if it exists
-  }
-
-  const polylineOptions = {
-    path: [departureLatLng, arrivalLatLng],
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  };
-
-  this.polyline = new google.maps.Polyline(polylineOptions);
-  if (this.polyline) {
-    this.polyline.setMap(this.map!);
-  }
-}
+  
+  
+  
 
    
   
