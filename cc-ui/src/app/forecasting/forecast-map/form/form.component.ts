6276d3071bc5ef0d117cc6782a4abc1d902d9eec
Vishrutha <128127959+Ivhandles@users.noreply.github.com>
2023-09-12 17:56:38 +0530

    Update form.component.ts

--- cc-ui/src/app/forecasting/forecast-map/form/form.component.ts
+++ cc-ui/src/app/forecasting/forecast-map/form/form.component.ts
@@ -1,15 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent  implements OnInit{
+
   @Input() portCode!: string;
   @Input() portId!: number;
   @Input() containersize!:number;
   @Input() containertype!:string;
   @Input() surplus!: number;
   @Input() deficit!: number 
+  @Input() surplusPercentage!: number; // New input
+  @Input() deficitPercentage!: number; // New input
+  @Input() surplusContainerTypesByPort!: string[];
+@Input() surplusContainerSizesByPort!: number[];
+@Input() DeficitContainerTypesByPort!: string[];
+@Input() DeficitlusContainerSizesByPort!: number[];
+ 
+
+  ngOnInit(): void {
+    console.log("to form",this.surplusPercentage)
+    console.log("to form",this.deficitPercentage)
+    console.log("to form",this.surplusContainerTypesByPort);
+    console.log("to form",this.surplusContainerSizesByPort);
+    console.log("to form",this.DeficitContainerTypesByPort);
+    console.log("to form",this.DeficitlusContainerSizesByPort);
+  }
 }
+
