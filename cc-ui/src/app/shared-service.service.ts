5ff8311a3891e6cfffaef1a05fc1c9e5c5713d25
Deekshith Kumar V <128376148+vdeekshithkumar@users.noreply.github.com>
2023-10-05 10:37:02 +0530

    Update shared-service.service.ts

--- cc-ui/src/app/shared-service.service.ts
+++ cc-ui/src/app/shared-service.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedServiceService {
-  private registeredEmailSource = new BehaviorSubject<string>(''); // Provide a default value here
+  private registeredEmailSource = new BehaviorSubject<string>('');
+  private PortSelected = new BehaviorSubject<string>('');
+  private selectedContainerType = new BehaviorSubject<string>('');
+  private selectedContainerSize = new BehaviorSubject<string>('');
   registeredEmail$ = this.registeredEmailSource.asObservable();
+  selected_port =this.PortSelected.asObservable();
+  selectedContainerType$ = this.selectedContainerType.asObservable();
+  selectedContainerSize$ = this.selectedContainerSize.asObservable();
 
   setRegisteredEmail(email: string) {
     this.registeredEmailSource.next(email);
     console.log("the email in shared service is"+email)
   }
+ setData(port_code: string, container_type: string,container_size:any) {
+    this.PortSelected.next(port_code);
+    this.selectedContainerType.next(container_type);
+    this.selectedContainerSize.next(container_size);
+    console.log("the port_code in shared service is " + port_code);
+  }
+
 }
