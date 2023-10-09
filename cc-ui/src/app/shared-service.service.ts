9b1fcc4a665e9452941687d530e9e28b09828018
Deekshith Kumar V <128376148+vdeekshithkumar@users.noreply.github.com>
2023-10-09 16:49:15 +0530

    Update shared-service.service.ts

--- cc-ui/src/app/shared-service.service.ts
+++ cc-ui/src/app/shared-service.service.ts
@@ -5,20 +5,41 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class SharedServiceService {
-  private registeredEmailSource = new BehaviorSubject<string>(''); // Provide a default value here
-  private dataSubject = new Subject<{ port_code: any; container_type: any ; container_size:any}>();
+  private registeredEmailSource = new BehaviorSubject<string>('');
+  private PortSelected = new BehaviorSubject<string>('');
+  private selectedContainerType = new BehaviorSubject<string>('');
+  private selectedContainerSize = new BehaviorSubject<string>('');
+  private portlatitude = new BehaviorSubject<number>(0);
+  private portlongitude = new BehaviorSubject<number>(0);
+  private deficitServicesData: any[] = [];
   registeredEmail$ = this.registeredEmailSource.asObservable();
- 
+  
+  selected_port =this.PortSelected.asObservable();
+  selectedContainerType$ = this.selectedContainerType.asObservable();
+  selectedContainerSize$ = this.selectedContainerSize.asObservable();
+  PortLatitude$ = this.portlatitude.asObservable();
+  PortLongitude$ = this.portlongitude.asObservable();
 
   setRegisteredEmail(email: string) {
     this.registeredEmailSource.next(email);
     console.log("the email in shared service is"+email)
   }
-  setData(port_code: any, container_type: any,container_size:any) {
-    this.dataSubject.next({ port_code, container_type,container_size });
-    console.log("Data emitted from service");
+ setData(port_code: string, container_type: string,container_size:any) {
+    this.PortSelected.next(port_code);
+    this.selectedContainerType.next(container_type);
+    this.selectedContainerSize.next(container_size);
+    console.log("the port_code in shared service is " + port_code);
   }
-  getData() {
-    return this.dataSubject.asObservable();
-  }
+setlatitudelongitude(latitude:any,longitude:any)
+{
+this.portlatitude.next(latitude);
+this.portlongitude.next(longitude);
+}
+setDeficitServices(data: any[]) {
+  this.deficitServicesData = data;
+}
+
+getDeficitServices() {
+  return this.deficitServicesData;
+}
 }
