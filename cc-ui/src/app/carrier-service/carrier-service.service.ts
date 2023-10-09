7863e15d7a9382bf3cbc17f6c71b64af69127d1d
Deekshith Kumar V <128376148+vdeekshithkumar@users.noreply.github.com>
2023-10-09 16:52:39 +0530

    Update carrier-service.service.ts

--- cc-ui/src/app/carrier-service/carrier-service.service.ts
+++ cc-ui/src/app/carrier-service/carrier-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,7 @@ import { Observable } from 'rxjs';
 export class CarrierServiceService {
   private carUrl='GetAllServicesByCompanyId';
   private sernameUrl='GetPortSequencesByServiceName';
+  private deficitServices = 'GetInternalServicePortSequenceData';
   constructor(private http: HttpClient,private apiService: ApiService) { }
   
   getCarrierServicesbyCId(companyId: number): Observable<any> {
@@ -22,4 +23,20 @@ export class CarrierServiceService {
     const url = this.apiService.getFullUrl(`${this.sernameUrl}?serviceName=${servicename}`);
     return this.http.get(url);
   }
+
+  getServicesforDeficit(companyId: number, portCode: string): Observable<Response> {
+    const url = this.apiService.getFullUrl(`${this.deficitServices}?companyId=${companyId}&portCode=${portCode}`);
+    
+    return this.http.get<Response>(url)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+  
+private handleError(error: any): Observable<never> {
+  // Implement your error handling logic here (e.g., logging, showing user-friendly messages).
+  console.error('An error occurred:', error);
+  throw error;
+}
+
 }
