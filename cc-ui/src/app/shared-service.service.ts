09dd583cd8c2be1a72ceaac4ef00bcfe03ff056d
Deekshith Kumar V <128376148+vdeekshithkumar@users.noreply.github.com>
2023-10-12 10:22:46 +0530

    Update shared-service.service.ts

--- cc-ui/src/app/shared-service.service.ts
+++ cc-ui/src/app/shared-service.service.ts
@@ -1,63 +1,87 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, zip } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedServiceService {
   private valuesSource = new BehaviorSubject<{
+    portcode: string;
+    containertype: string;
+    containersize: string;
+  }>({
+    portcode: '',
+    containertype: '',
+    containersize: '',
+  });
+  private valuesfordeficit = new BehaviorSubject<{
     portCode: string;
     containerType: string;
-    containerSize: string;
+    containerSize: number;
+    latitude:number;
+    longitude:number;
   }>({
     portCode: '',
     containerType: '',
-    containerSize: '',
+    containerSize: 0,
+    latitude:0,
+    longitude:0
   });
+  private valuesforsurplus = new BehaviorSubject<{
+    surplusportCode: string;
+    surpluscontainerType: string;
+    surpluscontainerSize: number;
+    surpluslatitude:number;
+    surpluslongitude:number;
+  }>({
+    surplusportCode: '',
+    surpluscontainerType: '',
+    surpluscontainerSize: 0,
+    surpluslatitude:0,
+    surpluslongitude:0
+  });
+ 
+  values$ = this.valuesSource.asObservable();
+  valuesforis$ = this.valuesfordeficit.asObservable();
+  valuesforsurplus$ = this.valuesforsurplus.asObservable();
+  
   private registeredEmailSource = new BehaviorSubject<string>('');
-  private PortSelected = new BehaviorSubject<string>('');
-  private selectedContainerType = new BehaviorSubject<string>('');
-  private selectedContainerSize = new BehaviorSubject<string>('');
-  private portlatitude = new BehaviorSubject<number>(0);
-  private portlongitude = new BehaviorSubject<number>(0);
-  private deficitServicesData: any[] = [];
+ 
   registeredEmail$ = this.registeredEmailSource.asObservable();
   
-  selected_port =this.PortSelected.asObservable();
-  selectedContainerType$ = this.selectedContainerType.asObservable();
-  selectedContainerSize$ = this.selectedContainerSize.asObservable();
-  PortLatitude$ = this.portlatitude.asObservable();
-  PortLongitude$ = this.portlongitude.asObservable();
-  values$ = this.valuesSource.asObservable();
+ 
 
   setRegisteredEmail(email: string) {
     this.registeredEmailSource.next(email);
     console.log("the email in shared service is"+email)
   }
- setData(port_code: string, container_type: string,container_size:any) {
-    this.PortSelected.next(port_code);
-    this.selectedContainerType.next(container_type);
-    this.selectedContainerSize.next(container_size);
-    console.log("the port_code in shared service is " + port_code);
-  }
-setlatitudelongitude(latitude:any,longitude:any)
-{
-this.portlatitude.next(latitude);
-this.portlongitude.next(longitude);
-}
-setDeficitServices(data: any[]) {
-  this.deficitServicesData = data;
-}
 
-getDeficitServices() {
-  return this.deficitServicesData;
-}
+ 
+
+  
 setValues(values: {
-  portCode: string;
-  containerType: string;
-  containerSize: string;
+  portcode: string;
+  containertype: string;
+  containersize: string;
 }) {
   this.valuesSource.next(values);
 }
-
+setisvaluesfordeficit(valuesforis:{
+  portCode: string;
+    containerType: string;
+    containerSize: number;
+    latitude:number;
+    longitude:number;
+}){
+  this.valuesfordeficit.next(valuesforis);
+}
+setisvaluesforsurplus(valuesforsurplus:{
+  surplusportCode: string;
+    surpluscontainerType: string;
+    surpluscontainerSize: number;
+    surpluslatitude:number;
+    surpluslongitude:number;
+}){
+  this.valuesforsurplus.next(valuesforsurplus);
+}
 }
