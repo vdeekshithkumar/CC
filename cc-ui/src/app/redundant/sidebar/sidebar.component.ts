e300ef66015f510366a7c60a56a82fabd25ba147
Deekshith Kumar V <128376148+vdeekshithkumar@users.noreply.github.com>
2023-10-20 14:20:22 +0530

    Update sidebar.component.ts

--- cc-ui/src/app/redundant/sidebar/sidebar.component.ts
+++ cc-ui/src/app/redundant/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, HostListener, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 
@@ -8,11 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  showPopup1: boolean = false;
+  showPopup2: boolean = false;
   showPopup : boolean =  false;
   isDropdownOpen: boolean = false;
   showPopupp: boolean  = false;
   popupMaxHeight: number = 150;
-  constructor(public dialog: MatDialog,private router: Router){}
+  constructor(public dialog: MatDialog,private router: Router,private el: ElementRef){}
   changeImageColor(event: MouseEvent) {
     const svg = (event.currentTarget as HTMLElement).querySelector('svg');
     if (svg) {
@@ -74,6 +76,16 @@ export class SidebarComponent {
   togglePopupp() {
     this.showPopupp = !this.showPopupp;
   }
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    if (!this.el.nativeElement.contains(event.target)) {
+      this.showPopup1 = false;
+      this.showPopup2 = false; // Close the pop-up when clicking outside of it
+    }
+  }
+
+
+  
   
   toggleDropdown(event: Event) {
     
@@ -85,6 +97,20 @@ export class SidebarComponent {
       this.popupMaxHeight = 300; // Change this value as needed
     } else {
       this.popupMaxHeight = 150; // Reset max-height when the dropdown is closed
+    }
+  }
+
+  togglePopup1() {
+    this.showPopup1 = !this.showPopup1;
+    if (this.showPopup1) {
+      this.showPopup2 = false; // Close the other popup when this one opens
+    }
+  }
+
+  togglePopup2() {
+    this.showPopup2 = !this.showPopup2;
+    if (this.showPopup2) {
+      this.showPopup1 = false; // Close the other popup when this one opens
     }
   }
 }
