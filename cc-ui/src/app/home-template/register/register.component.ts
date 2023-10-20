c5c745d86f0ee76416938f06abb3151d8ee53301
Deekshith Kumar V <128376148+vdeekshithkumar@users.noreply.github.com>
2023-10-20 10:33:29 +0530

    Update register.component.ts

--- cc-ui/src/app/home-template/register/register.component.ts
+++ cc-ui/src/app/home-template/register/register.component.ts
@@ -1,266 +1,293 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-@layer components {
-    .w-13 {
-      width: 8%;
-    }
-  }
-  @layer components {
-    .aspect-w-2 {
-      width: 2;
-    }
-  }
-  @layer components {
-    .aspect-h-1 {
-      height: 1;
-    }
-  }
-  /* .div-img{
-    @apply w-13 aspect-w-2 aspect-h-1 object-cover mix-blend-color-burn rounded-full ;
-  }
-   */
-.div{
-  @apply flex flex-row font-poppins;
-}
-.div-app-sidebar{
-    @apply sticky w-[8vw] ;
-  }
-.div-company-details{
-    @apply w-fit flex flex-col  first:flex-row ml-20 -mt-2;
-}
-.div-editcompany{
-    @apply absolute right-0 w-4 h-4 rounded-full top-8;
+import { Component,Inject, OnInit, Output,EventEmitter  } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Registerservice } from './register.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../../dialog.component';
+import { countries, Country } from 'countries-list';
+import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+interface RegisterResponse {
+  message: string;
+  user? : {
+  user_id:number;
+  otp:number;
+};
 }
 
-.div-company{
-    @apply px-3 w-fit text-xl mb-[0.625rem] font-bold font-poppins flex md:text-sm;
-}
-.div-company-name{
-    @apply text-xs font-thin pt-2 px-2;
-}
-.disabled-button {
-    cursor: not-allowed;
-  }
-.div-tag{
-    @apply bg-gray-100 w-fit h-[1rem]  flex flex-shrink-0 mt-[0.2rem]  justify-end rounded-md sm:text-[10px];
-}
-.div-detail{
-    @apply px-1 flex-1 text-sm md:overflow-hidden leading-3;
-}
-.forecast{
-    font-family: Poppins;
-font-size: 16px;
-line-height: 28px;
-}
-.btn{
-    padding: 10px 18px;
-    gap: 8px;
-    
-    width: 152.33px;
-    height: 48px;
-}
-.btn-primary{
-    color: #ffffff;
-    background-color: #2F54EB;
-    height: 38px;
-}
-.top{
-    font-family: 'Poppins';
-font-size: 12px;
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css','../../app.component.css']
+})
+export class RegisterComponent implements OnInit 
+{
+  email!:string;
+  countryList: { value: string; name: string; }[] = Object.values(countries)
 
-color: #595959;
-}
-.select{
-    font-family: 'Poppins';
-    color: #595959;
-    font-size: 12px;
-}
-.top-select{
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 10px 8px;
-    gap: 10px;
-    width: 150px;
-    height: 36px;
-    border: 1px solid #D9D9D9;
-}
-.top-selct2{
-box-sizing: border-box;
-display: flex;
-flex-direction: row;
-align-items: center;
-padding: 10px 8px;
-gap: 10px;
-width: 150px;
-height: 36px;
-border: 1px solid #D9D9D9;
-}
-.select-box{
-    border: 1px solid #D9D9D9;
-    box-sizing: border-box;
-    font-family: 'Poppins';
-    color: rgba(0, 0, 0, 0.88);
-    font-size: 10px;
-}
-.div-bg{
-    background: #FAFAFA;
-  }
-  .div-box-bg{
-    background: #F0F0F0;
-  }
-  .div-bg-line{
-    border-bottom: 1px solid #ADC6FF;
-  }
-.top-selct1{
-    box-sizing: border-box;
-display: flex;
-flex-direction: row;
-align-items: center;
-width: 150px;
-height: 36px;
-border: 1px solid #D9D9D9;
-gap: 10px;
-}
-.div-header-box{
-    height: 139px;
-width: 1751px;
-left: 165px;
-top: 60px;
-border-radius: 0px;
+  .sort((a, b) => a.name.localeCompare(b.name))
 
-}
+  .map((country: Country) => ({
 
-.btn-primary2{
-    color: #2F54EB;
-    height: 38px;
-}
-.btn-primary3{
-   background-color: #F0F0F0;
-    color:#595959;
-    height: 38px;
-}
-.div-sub-box{
-    @apply mx-[1rem] flex-shrink-0
-}
-.div-box{
-    @apply mx-2 w-[10em] h-[3.5em] bg-gray-100 flex;
-}
-.div-box-numbers{
-    @apply flex-1 text-3xl font-bold flex items-center justify-center;
-}
-.div-box-data{
-    @apply flex-1 text-sm flex items-center justify-center;
-}
-.div-box-number{
-    @apply flex-1 text-2xl flex-col font-bold flex items-center justify-center;
-}
-.div-employee{
-    @apply m-2 h-fit w-full flex-col;
-}
-.div-employee-box{
-    @apply flex-1 text-xs p-2 text-gray-500 flex align-middle justify-center w-fit;
-}
-.div-employee-name{
-    @apply text-xs px-2 first-letter:uppercase;
-}
-.div-action-button{
-    @apply bg-gray-300 h-[3rem] w-fit px-[1rem]  mx-4 my-3 md:text-sm flex-shrink-0;
-}
-.div-search{
-    @apply relative flex items-center w-40 h-9 focus-within:shadow-lg bg-gray-100 overflow-hidden;
-}
-.div-search-icon{
-    @apply grid place-items-center h-full w-12 text-black;
-}
-.input-search{
-    @apply h-full w-full outline-none text-sm bg-gray-100 text-black pr-2;
-}
-.employee-list{
-    @apply inline-block min-w-full py-2 sm:px-6 lg:px-8;
-}
-.employee-table{
-    @apply min-w-full text-left text-sm font-light;
-}
-.emplist-thead{
-    @apply border-b font-medium ;
-}
-.employee-add{
-    @apply border-b ;
-}
-.employee-add-btn{
-    @apply whitespace-nowrap px-2 py-4 font-medium;
-}
-.emplist{
-    @apply whitespace-nowrap px-6 py-4;
-}
-.radio-button {
-  @apply px-1 py-1 text-black border-gray-800 ml-2 w-20 h-8;
- }
- 
- .radio-button--selected {
-  @apply bg-blue-500 text-white border-blue-500;
- }
- .my-custom-select {
-   /* add your styles here */
-   font-size: 10px;
-   padding: 8px;
-   border-radius: 4px;
-   border: 1px solid #ccc;
- }
- .custom-box {
-    position: fixed;
-    top: 20px; /* adjust the top position as desired */
-    left: 20px; /* adjust the left position as desired */
-    width: 10em;
-    height: 7em;
-    background-color: #FFFFFF;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-    border-radius: 4px;
-  }
-  
-  .custom-label {
-    color: #666666;
-    font-weight: 500;
-    font-size: 14px;
-    margin-bottom: 4px;
-  }
+    value: country.name,
+
+    name: country.name
+
+  }));
   
 
-.label-contain {
-    display: grid;
-    align-items: center; 
-    grid-template-columns: 1fr 1fr 1fr;
-    column-gap: 5px;
+  @Output() emailSent = new EventEmitter<any>();
+  showValidationErrors: boolean = false;
+  company_id!:string;
+ checkEnabled:boolean = false;
+  firstName!: string;
+  lastName!: string;
+  address!:string;
+  agreeToTerms: boolean = false;  
+  phone_no!:number;
+  public user_id? : number;
+  public otp?:number;
+  registrationForm!: FormGroup;
+
+  emailFormControl! :FormGroup;
+  user_data:any;
+  form: any;
+  fetched_user_id:any;
+  userId:any;
+  Otp:any;
+   company_name= "";
+   company_list : any;
+   errors:any;
+  r: any;
+  
+  showPassword=false;
+  enteredCompanyName: string = '';
+  constructor(private snackBar: MatSnackBar,private route: ActivatedRoute,private formBuilder: FormBuilder,private dialog: MatDialog,private router:Router,private registerservice:Registerservice) {
    }
-   .popup {
-    position: absolute;
-    top: 30%;
-    left: calc(100% + 10px); /* Position to the right of the Container button with some spacing (adjust as needed) */
-    width: 120px;
-    height: auto; /* Allow the height to adjust based on content */
-    max-height: 150px; /* Limit the maximum height as needed */
-    background-color: rgba(0, 0, 0, 0.5); /* Set the background color to opaque white (#ffffff) */
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-    transition: left 0.3s ease-in-out; /* Use left for transition */
-    overflow-y: auto; /* Add vertical scrollbar if content exceeds max height */
-    z-index: 2000;
+
+
+ngOnInit(): void {
+ 
+  const now = new Date();
+  const formattedDateTime = now.toISOString();
+  console.log(formattedDateTime); // Output: "2023-07-26T12:34:56.789Z"
+   // get date in format yyyy-mm-dd
+  this.registrationForm = this.formBuilder.group({
+    user_id: ['0',Validators.required],
+    company_id:['',Validators.required],
+    first_name: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.maxLength(20)]],
+    last_name: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.maxLength(20)]],
+    address: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(25)]],
+    phone_no: ['', [Validators.required, this.validPhoneNumber()]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$'), Validators.maxLength(15)]],
+    city:['', [Validators.pattern('[a-zA-Z ]*'), Validators.maxLength(10)]],
+    otp:['123456',Validators.required],
+    is_verified:[false,Validators.required],
+    is_approved:[true,Validators.required],
+    is_active:[true,Validators.required],
+    last_login:formattedDateTime,
+    designation: ['admin',Validators.required],
+    iagree: [false, Validators.requiredTrue]
+  });
+
+
+  this.registerservice.getAllCompanies().subscribe(
+    data => {
+      this.company_list = data;
+      console.log("port list fetched: ", this.company_list); 
+    },
+    error => {
+      console.log("Company loading error: "+ error);
+    }
+  );
+
+}
+validPhoneNumber(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const phoneNumberPattern = /^[0-9]+$/; // Regular expression to allow only numbers
+
+    if (control.value && !phoneNumberPattern.test(control.value)) {
+      return { invalidPhoneNumber: true };
+    }
+
+    return null;
+  };
+}
+onCompanyNameChange(event: Event) {
+  // Explicitly specify the event type as 'Event' and then access the 'value' property of the target element.
+  this.enteredCompanyName = (event.target as HTMLInputElement).value;
+}
+
+onSubmit(): void {
+  debugger;
+  this.showValidationErrors = true;
+
+  // Find the corresponding company_id based on the entered company name.
+  const selectedCompany = this.company_list.find((company: { name: string }) => company.name === this.enteredCompanyName);
+
+  if (selectedCompany) {
+    // Set the company_id in the form control.
+    this.registrationForm.patchValue({ company_id: selectedCompany.company_id });
+  } else {
+    // Handle the case when the entered company name is not found in the list.
+    this.openErrorDialog('Invalid Company Name');
+    return;
+  }
+  const formValue = this.registrationForm.value;
+  if (
+    !formValue.first_name ||
+    !formValue.last_name ||
+    !formValue.email ||
+    !formValue.address ||
+    !formValue.iagree||
+    !this.enteredCompanyName ||
+    !formValue.password
+  ) {
+    this.showValidationErrors = true;
+    let errorMessage = 'The following fields are required:\n';
+    if (!formValue.first_name) {
+      errorMessage += '- First Name\n';
+    }
+    if (!formValue.last_name) {
+      errorMessage += '- Last Name\n';
+    }
+    if (!formValue.email) {
+      errorMessage += '- Email\n';
+    }
+    if (!formValue.address) {
+      errorMessage += '- Country\n';
+    }
+    if (!formValue.enteredCompanyName) {
+      errorMessage += '- Company Name\n';
+    }
+    if (!formValue.password) {
+      errorMessage += '- Password\n';
+    }
+    this.openErrorDialog(errorMessage);
+    
+    return;
+  }
+
+  if (!this.registrationForm.controls['email'].valid) {
+    if (this.registrationForm.controls['email'].hasError('email')) {
+      this.openErrorDialog('Invalid email format');
+    } else if (this.registrationForm.controls['email'].hasError('maxlength')) {
+      this.openErrorDialog('Maximum character limit exceeded');
+    }
+    return;
+  }
+
+  if (!this.registrationForm.controls['address'].valid) {
+    this.openErrorDialog('Invalid Country Name');
+    return;
+  }
+
+  
+if (!this.registrationForm.controls['first_name'].valid) {
+  if (this.registrationForm.controls['first_name'].hasError('maxlength')) {
+    this.openErrorDialog('Maximum character limit exceeded for First Name');
+  } else {
+    this.openErrorDialog('Invalid First Name Format');
+  }
+  return;
+}
+  if (!this.registrationForm.controls['last_name'].valid) {
+    if (this.registrationForm.controls['last_name'].hasError('maxlength')) {
+      this.openErrorDialog('Maximum character limit exceeded for Last Name');
+    } else {
+      this.openErrorDialog('Invalid Last Name Format');
+    }
+    return;
+  }
+  const passwordControl = this.registrationForm.get('password');
+  if (passwordControl && passwordControl.invalid) {
+    this.showValidationErrors = true;
+    let passwordErrorMessage = 'Invalid password:\n';
+    if (passwordControl.errors?.['required']) {
+      passwordErrorMessage += '- Password is required\n';
+    }
+    if (passwordControl.errors?.['minlength']) {
+      passwordErrorMessage += '- Password must be at least 8 characters long\n';
+    }
+    if (passwordControl.errors?.['maxlength']) {
+      passwordErrorMessage += '- Password cannot exceed 15 characters\n'; // Add your desired max limit message
+    }
+    if (passwordControl.errors?.['pattern']) {
+      passwordErrorMessage += '- Password must contain at least one uppercase letter, one lowercase letter, and one digit\n';
+    }
+    this.openErrorDialog(passwordErrorMessage);
+    return;
   }
   
-  .popup-content{
-    background-color: #ffffff;
+  if (!this.registrationForm.controls['phone_no'].valid) {
+    if (this.registrationForm.controls['phone_no'].hasError('maxlength')) {
+      this.openErrorDialog('Maximum character limit exceeded');
+    } else{
+      this.openErrorDialog('Invalid format');
+    }
+    return;
   }
-  .popup::before {
-    content: '';
-    position: absolute;
-    top: 50%; /* Vertically centered */
-    left: -10px; /* Adjust the position */
-    width: 0;
-    height: 0;
-    border-top: 10px solid transparent;
-    border-bottom: 10px solid transparent;
-    border-right: 10px solid #ffffff; /* Color matches the background */
+
+  if (!this.registrationForm.controls['city'].valid) {
+    if (this.registrationForm.controls['city'].hasError('city')) {
+      this.openErrorDialog('Invalid format');
+    } else if (this.registrationForm.controls['city'].hasError('maxlength')) {
+      this.openErrorDialog('Maximum character limit exceeded');
+    }
+    return;
   }
+  
+  try {
+    const response = this.registerservice.register(formValue).toPromise();
+    this.snackBar.open('OTP Sent Successfully, Please verify your Email', 'OK', {
+      duration: 3000,
+           verticalPosition: 'top',
+    });
+    this.email = this.registrationForm.value.email;
+    this.emailSent.emit(this.email);
+    console.log(this.email+"email emiting");
  
+    console.log(response);
+    console.log(formValue);
+    this.router.navigate(['/otp-validation'])
+  } catch (error) {
+    console.log('Error registering:', error);
+  }
+
+}
+
+
+
+
+private redirect(){
+
+  
+    this.router.navigate(['/sign-in'])
+  
+
+}
+
+ changeInputType(input: { setAttribute: (arg0: string, arg1: any) => void; }, type: any) {
+  setTimeout(function() {
+    input.setAttribute('type', type);
+  }, 100);
+}
+
+
+openErrorDialog(message: string): void {
+  this.dialog.open(DialogComponent, {
+    data: {
+      message: message
+    }
+  });
+}
+toggleCheckEnabled():void{
+  this.checkEnabled = !this.checkEnabled;
+  console.log("dfd"+ this.checkEnabled);
+}
+togglePasswordVisibility() {
+  this.showPassword = !this.showPassword;
+}
+
+
+}
