d01fc8ad95abb452deec5b04f8c3c20c2784c5ce
Deekshith Kumar V <128376148+vdeekshithkumar@users.noreply.github.com>
2024-07-03 10:17:29 +0530

    Update api.service.ts

--- cc-ui/src/app/api.service.ts
+++ cc-ui/src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-const BASE_URL = 'https://contanerc-api.azurewebsites.net/';
+const BASE_URL = 'https://ccapi20240703100113.azurewebsites.net/swagger/index.html';
 //const BASE_URL ='https://localhost:7157/';
 @Injectable({
   providedIn: 'root'
