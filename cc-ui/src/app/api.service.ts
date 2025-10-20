4a617062e23a2b15f1c90635bef15fd21f69272b
Deekshith Kumar V <128376148+vdeekshithkumar@users.noreply.github.com>
2025-10-20 18:54:51 +0530

    Update api.service.ts

--- cc-ui/src/app/api.service.ts
+++ cc-ui/src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-const BASE_URL = 'https://ccapi.azurewebsites.net/';
+const BASE_URL = 'https://containerconundrum-api-dev.platformnx.com/';
 //const BASE_URL ='https://localhost:7157/';
 @Injectable({
   providedIn: 'root'
